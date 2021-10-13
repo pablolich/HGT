9f9a7b73c6a51183a2dc984f70fdb0a61ff0d16c
Pablo Lechon <plechon@ucm.es>
2021-10-13 15:43:12 -0500

    finished with code without HGT and enzyme budget

--- code/preliminar_results.py
+++ code/preliminar_results.py
@@ -50,6 +50,7 @@ def invasion_criteria(r_i, A, x):
     except:
         #If A is a scalar because we are in the first iteration
         a_i_vec = A
+    inv = r_i + np.dot(a_i_vec, x)
     try:
         if len(inv) > 1:
             import ipdb; ipdb.set_trace(context = 20)
@@ -58,10 +59,13 @@ def invasion_criteria(r_i, A, x):
     return inv 
 
 def find_equilibria(r, A):
-    try:
-        x_star = (-np.linalg.inv(A)@r.reshape(len(r), 1)).T[0]
-    except:
+    if isinstance(A, float) or isinstance(A, int):
         x_star = -1/A*r
+    else:
+        try:
+            x_star = (-np.linalg.inv(A)@r.reshape(len(r), 1)).T[0]
+        except: 
+            x_star = None
     return x_star
 
 def find_eigenvals(A):
@@ -130,6 +134,8 @@ def main(argv):
     P = v_first
     #Set abundance threshold
     ab_thresh = 1e-6
+    #Initialize number of species vector
+    n_sp_vec = list()
     #Start iteration
     while n_sp <= m:
         print(n_sp)
@@ -144,19 +150,25 @@ def main(argv):
         A_new = interaction_matrix(P_new)
         #Check if the new species can invade
         inv = invasion_criteria(r, A_new, x_star)
-        print(inv)
+        print('inv criteria: ', inv)
         if inv > 0:
             #In the case that it can invade, add it to the community 
             P = P_new
             A = interaction_matrix(P)
             #Update number of species
             n_sp += 1
+            n_sp_vec.append(n_sp)
             #Find new equilibrium
             x_star_new = find_equilibria(np.ones(n_sp), A)
             #Calculate eigenvalues of A
             eigen_vals_new = find_eigenvals(A)
             #Check feasibility and stability
-            feasible = np.all(x_star_new > 0) 
+            try:
+                feasible = np.all(x_star_new > 0) 
+            except:
+                plt.plot(n_sp_vec)
+                plt.show()
+                raise Exception('A is singular, end loop')
             stable = np.all(eigen_vals_new.real < 0)
             if feasible and stable:
                 #Set x_star to the equilibrium reached in the presecne of
@@ -171,14 +183,13 @@ def main(argv):
                 while not (feasible and stable):
                     #Integrate until solutions are constant 
                     constant_sol = False
-                    while not constant_sol:
+                    #Put a maximum integration times
+                    runs = 0
+                    while not constant_sol and runs < 10:
                         #Run dynamics until putative equilibrium is reached
                         sol = solve_ivp(lotka_volterra, t_span, x0, 
                                         method = 'BDF', 
                                         args = (A,  np.ones(n_sp)))
-                        for i in range(n_sp):
-                            plt.plot(sol.t, sol.y[i, :])
-                        plt.show()
                         #Check if solution is constant
                         constant_sol = check_constant(sol.y, tol = 1e-3)
                         #Get rid of extinct species (those below abundance 
@@ -191,11 +202,18 @@ def main(argv):
                         #endpoints of the previous one
                         x0 = x_star
                         n_sp = len(x_star)
+                        runs += 1
+                        print('Integration number: ', runs)
                     #Check feasibility and stability
                     feasible = np.all(x_star > 0) 
                     eigen_vals_new = np.linalg.eigvals(A)
                     stable = np.all(eigen_vals_new.real < 0)
                     n_sp_new = len(x_star)
+                #We pruned the community succesfully to a feasible and stable
+                #state
+                #Record species number
+                n_sp_vec.append(len(x_star))
+                print(n_sp_vec)
         else: 
             #Invasibility criterion is not satisfied, so come up with another 
             #mutant that can invade
